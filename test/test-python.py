ea66da5821ca50cc5d14e7a1e6da89ed706c017a
JohnnyFontanilla <109713487+JohnnyFontanilla@users.noreply.github.com>
2022-07-20 23:22:23 -0500

    Add files via upload

---
+++ test/test-python.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -- coding: utf-8 --
+# **********
+# *       BullDocker         *
+# * Date  : 18/05/2019       *
+# * Author: ADL DevOps Team  *
+# **********
+
+from sys import exit
+import main
+
+
+if __name__ == "__main__":
+    main.run()
+    exit()
+
+
+
